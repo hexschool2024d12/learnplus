9d9a2f9151fdeddff0d6ebbda8d975b7998a353e
joeychen0516 <chen610409@gmail.com>
2024-11-02 19:19:53 +0800

    update

---
+++ assets/Js/index.js
@@ -0,0 +1,24 @@
+//clockIN
+const clockIN = document.querySelector('.clockIN');
+clockIN.addEventListener("click",function(e){
+    clockIN.setAttribute("src","/assets/images/sign-in-done.png");
+})
+
+//signIN
+let signIn = document.querySelector('.signIN');
+signIn.addEventListener("click",function(e){
+    let str=`<a href="#" class="btn btn-primary text-nowrap rounded-0 radius-bottom-l border-end-dark btn-border-top nav-btn-hover nav-hover fw-medium w-100"><i class="bi bi-cart-fill"></i>購物車</a>
+  <div class="btn-group dropup">
+      <a href="#" class="btn btn-primary dropdown-up text-nowrap rounded-0 radius-bottom-r btn-border-top nav-btn-hover fw-medium" data-bs-toggle="dropdown"><i class="bi bi-person-fill"></i>帳戶</a>
+      <ul class="dropdown-menu dropdown-menu-end me-3 mb-3 p-3 border-2px">
+          <li class="py-2 px-5"><a href="personal.html" class="text-gray-60 li-hover"><i class="bi bi-person-circle"></i> 個人資料</a></li>
+          <li class="py-2 px-5"><a href="" class="text-gray-60 li-hover"><i class="bi bi-star"></i> 積分紀錄</a></li>
+          <li class="py-2 px-5"><a href="" class="text-gray-60 li-hover"><i class="bi bi-bell"></i> 訊息通知</a></li>
+          <li class="py-2 px-5 border-bottom-black"><a href="" class="text-gray-60 li-hover"><i class="bi bi-gear"></i> 設定</a></li>
+          <li class="py-2 px-5 logOut"><a href="" class="text-gray-60 li-hover"><i class="bi bi-box-arrow-right"></i> 登出</a></li>
+      </ul>
+  </div>`
+    signIn.innerHTML= str;
+    signIn="";
+});
+
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import './assets/Js/index.js';
 import './assets/Js/swiper.js';
 
 console.log("Hello world!");
